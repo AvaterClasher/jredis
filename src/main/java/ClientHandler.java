ee41df11a001d3b66140a2acc5d0fc64b530bf9a
AvaterClasher <avater.clasher@gmail.com>
2024-03-09 01:13:00 +0530

    feat(Client_Handler): :sparkles: Implemented array list to parse and execute the requests

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientHandler implements Runnable {
     private final Socket clientSocket;
@@ -18,13 +20,38 @@ public class ClientHandler implements Runnable {
             String input;
             while ((input = in.readLine()) != null) {
                 System.out.println("command received " + input);
-                if ("PING".equalsIgnoreCase(input)) {
-                    out.print("+PONG\r\n");
-                    out.flush();
+                if (input.startsWith("*")) {
+                    int numberOfItems = Integer.parseInt(input.substring(1));
+                    List<String> commands = new ArrayList<>(numberOfItems * 2); // size of command
+                    for (int i = 0; i < numberOfItems * 2; i++) {
+                        commands.add(in.readLine());
+                    }
+
+                    switch (commands.get(1).toLowerCase()) {
+                        case Constants.PING:
+                            sendResponse(new Ping().execute(""));
+                            break;
+                        case Constants.ECHO:
+                            sendResponse(new Echo().execute(commands.get(3)));
+                            break;
+                        default:
+                            sendResponse("invalid commands");
+                    }
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void sendResponse(String response) {
+        try {
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            out.print("+PONG\r\n");
+            out.print(response);
+            out.flush();
+        } catch (IOException ex) {
+            throw new RuntimeException("caught error while sending data to clients");
         }
     }
 
