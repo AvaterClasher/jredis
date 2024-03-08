e4e8f2d0d3f167855ebbf8b8fa20d56f62171ae9
AvaterClasher <avater.clasher@gmail.com>
2024-03-09 00:42:25 +0530

    feat(Client_Handler): :sparkles: Added a thread pool for making it multithreaded

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 class Main {
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(5);
     public static void main(String[] args) {
         System.out.println("Logs from your program will appear here!");
+
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
